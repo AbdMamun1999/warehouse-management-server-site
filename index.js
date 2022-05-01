d4ad30f64f43e4af36395b36be7743bad6cd1d3c
AbdMamun1999 <mamunrox@gmail.com>
2022-05-01 12:31:32 +0600

    server connect with mongodb database and add env file

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9tgh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('db connected')
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req,res)=>{
     res.send('start warehouse management server')
