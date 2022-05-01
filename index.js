2612f69bbd1002231fea0c7e388d6dcbcae577cd
AbdMamun1999 <mamunrox@gmail.com>
2022-05-01 18:25:24 +0600

    get single data and data api is added

--- index.js
+++ index.js
@@ -9,25 +9,37 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9tgh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
+async function run() {
 
-    try{
+    try {
         await client.connect();
         const inventoryCollection = client.db('warehouse-management').collection('inventory')
 
         // get all data from database
-        app.get('/inventory',async (req,res)=>{
+        app.get('/inventory', async (req, res) => {
             const query = {}
-            const cursor =  inventoryCollection.find(query).limit(6)
+            const cursor = inventoryCollection.find(query).limit(6)
             const inventories = await cursor.toArray();
             res.send(inventories)
 
         })
+
+        // get single data with id
+        app.get('/inventory/:id',async (req,res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result = await inventoryCollection.findOne(query)
+            res.send(result)
+        })
+
+        
+
+
     }
-    finally{}
+    finally { }
 
 }
 
@@ -39,10 +51,10 @@ run().catch(console.dir)
 }); */
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('start warehouse management server')
 })
 
-app.listen(port,()=>{
-    console.log('warehouse management port:',port)
+app.listen(port, () => {
+    console.log('warehouse management port:', port)
 })
