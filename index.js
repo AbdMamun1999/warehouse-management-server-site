3e387d4f986b20d7ecd063340bf9e966321fd0be
Abdullah Al Mamun <mamun12121999@gmail.com>
2022-12-10 10:53:36 +0600

    some change

--- index.js
+++ index.js
@@ -1,131 +1,134 @@
-const express = require('express');
-const cors = require('cors')
-const jwt = require('jsonwebtoken');
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 function authorization(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ massage: 'unauthorized aceess' })
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ massage: "unauthorized aceess" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ massage: "Forbiden access" });
     }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ massage: 'Forbiden access' })
-        }
-        req.decoded = decoded;
-        next();
-    })
-    
+    req.decoded = decoded;
+    next();
+  });
 }
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9tgh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9tgh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client
+      .db("warehouse-management")
+      .collection("inventory");
 
-    try {
-        await client.connect();
-        const inventoryCollection = client.db('warehouse-management').collection('inventory')
+    // auth
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
-        // auth 
-        app.post('/login', async (req, res) => {
-            const user = req.body
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1d'
-            });
-            res.send({ accessToken })
-        })
+    // get all data from database
+    app.get("/inventories", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
 
-        // get all data from database
-        app.get('/inventories', async (req, res) => {
-            const query = {}
-            const cursor = inventoryCollection.find(query)
-            const inventories = await cursor.toArray();
-            res.send(inventories)
-        })
+    // search by email
+    app.get("/myItems", authorization, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = inventoryCollection.find(query);
+        const inventories = await cursor.toArray();
+        res.send(inventories);
+      } else {
+        res.status(403).send({ massage: "Forbiden access" });
+      }
+    });
 
-        // search by email
-        app.get('/myItems', authorization, async (req, res) => {
-            const decodedEmail = req.decoded.email;
-            const email = req.query.email;
-            if (email === decodedEmail) {
-                const query = { email: email }
-                const cursor = inventoryCollection.find(query)
-                const inventories = await cursor.toArray();
-                res.send(inventories)
-            }
-            else {
-                res.status(403).send({ massage: 'Forbiden access' })
-            }
-        })
+    // get 6 data from database
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query).limit(6);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
 
-        // get 6 data from database
-        app.get('/inventory', async (req, res) => {
-            const query = {}
-            const cursor = inventoryCollection.find(query).limit(6)
-            const inventories = await cursor.toArray();
-            res.send(inventories)
+    // get single data with id
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.findOne(query);
+      res.send(result);
+    });
 
-        })
+    // const update data with id
+    app.put("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateQuantity = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedQuantity = {
+        $set: {
+          quantity: updateQuantity.currentQuantity,
+        },
+      };
+      const result = await inventoryCollection.updateOne(
+        filter,
+        updatedQuantity,
+        options
+      );
+      res.send(result);
+    });
 
-        // get single data with id
-        app.get('/inventory/:id', async (req, res) => {
-            const id = req.params.id
-            const query = { _id: ObjectId(id) }
-            const result = await inventoryCollection.findOne(query)
-            res.send(result)
-        })
+    // Delete Data with id
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    });
 
-        // const update data with id
-        app.put('/inventory/:id', async (req, res) => {
-            const id = req.params.id
-            const updateQuantity = req.body;
-            const filter = { _id: ObjectId(id) }
-            const options = { upsert: true };
-            const updatedQuantity = {
-                $set: {
-                    quantity: updateQuantity.currentQuantity,
-                }
-            }
-            const result = await inventoryCollection.updateOne(filter, updatedQuantity, options)
-            res.send(result)
-        })
-
-        // Delete Data with id
-        app.delete('/inventory/:id', async (req, res) => {
-            const id = req.params.id
-            const query = { _id: ObjectId(id) }
-            const result = await inventoryCollection.deleteOne(query)
-            res.send(result)
-        })
-
-        // post data in database
-        app.post('/inventory', async (req, res) => {
-            const img = req.body
-            const result = await inventoryCollection.insertOne(img)
-            res.send(result)
-        })
-
-
-    }
-    finally { }
-
+    // post data in database
+    app.post("/inventory", async (req, res) => {
+      const img = req.body;
+      const result = await inventoryCollection.insertOne(img);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('start warehouse management server')
-})
+app.get("/", (req, res) => {
+  res.send("start warehouse management server");
+});
 
 app.listen(port, () => {
-    console.log('warehouse management port:', port)
-})
+  console.log("warehouse management port:", port);
+});
